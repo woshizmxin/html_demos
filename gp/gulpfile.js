1d7da4e96e0843a7bf27b939faf8b5927fd3b7c4
zhoumao <zhoumao@meituan.com>
2017-06-15 13:53:14 +0800

    add rem transform tools.
    Signed-off-by: zhoumao <zhoumao@meituan.com>

--- gp/gulpfile.js
+++ gp/gulpfile.js
@@ -1,54 +1,51 @@
 var gulp = require('gulp'),
-    rev = require('gulp-rev-append');
-replace = require('gulp-replace');
-htmlmin = require('gulp-htmlmin');
-clean = require('gulp-clean');
-cssmin = require('gulp-cssmin');
-rename = require('gulp-rename');
-uglify = require('gulp-uglify');
+    revappend = require('gulp-rev-append'),
+    assetRev = require('gulp-asset-rev'),
+    runSequence = require('run-sequence'),
+    replace = require('gulp-replace'),
+    htmlmin = require('gulp-htmlmin'),
+    clean = require('gulp-clean'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    revCollector = require('gulp-rev-collector'),
+    rev = require('gulp-rev');
 
+//定义css、js源文件路径
+var cssSrc = 'src/public/*.css',
+    jsSrc = 'src/js/*.js';
 
-gulp.task('default', ['clean'], function () {
-    gulp.start('minHashHtml','mincss','minjs','img','fonts');
+//开发构建
+gulp.task('default',['clean'], function (done) {
+    runSequence(    //需要说明的是，用gulp.run也可以实现以上所有任务的执行，只是gulp.run是最大限度的并行执行这些任务，而在添加版本号时需要串行执行（顺序执行）这些任务，故使用了runSequence.
+        [ 'mincss', 'minjs', 'img', 'fonts'],
+        ['revCss'],
+        ['revJs'],
+        ['revHtml'],
+        done);
 });
 
 
-gulp.task('clean', function () {
-    return gulp.src('dest/', {read: false})
-        .pipe(clean());
+//CSS生成文件hash编码并生成 rev-manifest.json文件名对照映射
+gulp.task('revCss', function () {
+    return gulp.src(cssSrc)
+        .pipe(rev())
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/css'));
 });
 
 
-//压缩js文件
-gulp.task('minjs',function(){
-    gulp.src('src/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dest/js'));
+//js生成文件hash编码并生成 rev-manifest.json文件名对照映射
+gulp.task('revJs', function () {
+    return gulp.src(jsSrc)
+        .pipe(rev())
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/js'));
 });
 
 
-//压缩css文件
-gulp.task('mincss',function(){
-    gulp.src('src/public/*.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('dest/css'));
-});
-
-//转移img文件
-gulp.task('img',function(){
-    gulp.src('src/images/*')
-        .pipe(gulp.dest('dest/images'));
-});
-
-//转移fonts文件
-gulp.task('fonts',function(){
-    gulp.src('src/fonts/*')
-        .pipe(gulp.dest('dest/fonts'));
-});
-
-
-// 压缩html及其中的js,css代码; 添加hash后缀;
-gulp.task('minHashHtml', function () {
+//Html替换css、js文件版本
+gulp.task('revHtml', function () {
     var options = {
         removeComments: true,//清除HTML注释
         collapseWhitespace: true,//压缩HTML
@@ -59,12 +56,48 @@ gulp.task('minHashHtml', function () {
         minifyJS: true,//压缩页面JS
         minifyCSS: true//压缩页面CSS
     };
-    return gulp.src('src/**/*.html')
-        .pipe(replace("_Public_", "public"))
-        .pipe(rev())
-        .pipe(replace("public", "_Public_"))
+    return gulp.src(['rev/**/*.json', 'src/**/*.html'])
+        .pipe(revCollector())
         .pipe(htmlmin(options))
-        .pipe(gulp.dest('dest/'));
+        .pipe(gulp.dest('dest'));
 });
+
+
+////////////////////////////////////////////////////////
+
+gulp.task('clean', function () {
+    return gulp.src('dest/', {read: false})
+        .pipe(clean());
+});
+
+
+//压缩js文件
+gulp.task('minjs', function () {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dest/js'));
+});
+
+
+//压缩css文件
+gulp.task('mincss', function () {
+    gulp.src('src/public/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('dest/css'));
+});
+
+//转移img文件
+gulp.task('img', function () {
+    gulp.src('src/images/*')
+        .pipe(gulp.dest('dest/images'));
+});
+
+//转移fonts文件
+gulp.task('fonts', function () {
+    gulp.src('src/fonts/*')
+        .pipe(gulp.dest('dest/fonts'));
+});
+
+
 
 
